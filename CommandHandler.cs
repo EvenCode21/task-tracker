9653b0d3eb9d153f97e2efcc4630dd63fd427872
EvenCode21 <evencode21@gmail.com>
2024-12-07 15:36:59 -0400

    First advance

---
+++ CommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_Tracker_CLI
+{
+    class CommandHandler
+    {
+        public void ProcessCommands(string inputText)
+        {
+            string[] text = TextProcessor.GetCommands(inputText);
+
+            //we're checking this to handle an array index err
+            if (text[0] == "task-cli" && text.Length >= 2)
+            {
+                switch (text[1])
+                {
+                    case "add":
+                        Console.WriteLine($"Adding the task");
+                        break;
+                    case "remove":
+                        Console.WriteLine("Removing task");
+                        break;
+                    default:
+                        Console.WriteLine("Command not recognized");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("err");
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_Tracker_CLI
+{
+    class Program
+    {
+        static CommandHandler commandHandler;
+        static void Main(string[] args)
+        {
+            //first make a simple CLI that accept user inputs and arguments
+            commandHandler = new CommandHandler();
+
+            Console.WriteLine("Welcome to the task tracker!\nplease enter the command task-cli to start\n");
+            commandHandler.ProcessCommands(Console.ReadLine());
+        }
+    }
+}
